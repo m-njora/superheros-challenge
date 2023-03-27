63cf051f839126b61812b75c70fbe27692db63c0
m-njora <mmuthekijora@gmail.com>
2023-03-27 13:19:32 +0300

    added relationship to hero serializers

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -11,8 +11,8 @@ class HeroesController < ApplicationController
   def show
      hero = Hero.find_by(id: params[:id])
      if hero
-      render json: hero.as_json(include: :powers)
-     else
+      render json: hero, each_serializer: HeroSerializer
+    else
       render json: { error: 'Hero not found'}, status: :not_found
      end
   end
--- app/serializers/hero_serializer.rb
+++ app/serializers/hero_serializer.rb
@@ -1,4 +1,5 @@
 class HeroSerializer < ActiveModel::Serializer
   attributes :id, :name, :super_name
+  has_many :powers, through: :hero_powers
 
 end
