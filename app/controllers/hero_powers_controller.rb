0a00940a4eccc583e82d512b4aee234a5ceba80b
m-njora <mmuthekijora@gmail.com>
2023-03-27 11:36:39 +0300

    post request to hero powers fixed

---
+++ app/controllers/hero_powers_controller.rb
@@ -0,0 +1,21 @@
+class HeroPowersController < ApplicationController
+
+# POST /hero_powers
+
+    def create
+        hero_power = HeroPower.new(hero_power_params)
+
+        if hero_power.save
+            render json: hero_power.hero.as_json(include: :powers)
+
+        else
+            render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+    def hero_power_params
+        params.require(:hero_power).permit(:strength, :power_id, :hero_id)
+      end
+end    
