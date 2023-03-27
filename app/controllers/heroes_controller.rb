77d7fb46822fc0c2ba965f1122ea2b2f6865b172
m-njora <mmuthekijora@gmail.com>
2023-03-27 06:16:01 +0300

    set up get/heros/:id  route

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -9,6 +9,12 @@ class HeroesController < ApplicationController
   #GET /heroes/:id
 
   def show
+     hero = Hero.find_by(id: params[:id])
+     if hero
+      render json: hero.as_json(include: :powers)
+     else
+      render json: { error: 'Hero not found'}, status: :not_found
+     end
   end
 
 end
