d9e79943fdf43838956797704159b35a1e1a4c23
m-njora <mmuthekijora@gmail.com>
2023-03-27 11:23:59 +0300

    done with the patch request

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -11,13 +11,31 @@ class PowersController < ApplicationController
   def show
     power = Power.find_by(id: params[:id])
      if power
-      render json: power.as_json
+      render json: power
      else
       render json: { error: 'Power not found'}, status: :not_found
      end
   end
   
 
-  def change
+    def update
+      power = Power.find_by(id: params[:id])
+  
+      if power
+        if power.update(power_params)
+          render json: power, status: :ok
+        else
+          render json: { errors: power.errors.full_messages }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: 'Power not found' }, status: :not_found
+      end
+    end
+  
+    private
+  
+    def power_params
+      params.require(:power).permit(:description)
+    end
+  
   end
-end
